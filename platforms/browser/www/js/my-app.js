2add82b704472e3208fcc6a85eeac336ef435bbb
Julie <tuyetnganphamhoang@gmail.com>
2018-10-24 02:29:26 +0700

    finish hardware

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -6,7 +6,7 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
-$$(document).on('pageInit', '.page[data-page="result"]', function (e) {
+$$(document).on('pageInit', '.page[data-page="result-device-info"]', function (e) {
     let $message = "";
     $message += "Cordova version: " + device.cordova;
     $message += "<br/>Device Model: " + device.model;
@@ -16,31 +16,86 @@ $$(document).on('pageInit', '.page[data-page="result"]', function (e) {
     $message += "<br/>Manufacturer: " + device.manufacturer;
     $message += "<br/>Serial: " + device.serial;
     $message += "<br/>is Virtual: " + device.isVirtual;
-    document.getElementById('resultContent').innerHTML = $message;
-    
+    document.getElementById('resultDeviceInfo').innerHTML = $message;
 });
-$$(document).on('pageInit', '.page[data-page="compass"]', function (e) {
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-        alert(navigator.compass);
-        var watchID = navigator.compass.getCurrentHeading(onSuccess, onError, options);    
-    }
+$$(document).on('pageInit', '.page[data-page="compass"]', function () {  
+    var btnStopCompass = document.getElementById('btnStopCompass');
+    btnStopCompass.addEventListener('click', function(e) {
+        e.preventDefault();
+        navigator.compass.clearWatch(watchID);
+    }, false);
+    var btnPlayCompass = document.getElementById('btnPlayCompass');
+    btnPlayCompass.addEventListener('click', function(e) {
+        e.preventDefault();
+        watchID = navigator.compass.watchHeading(onSuccess, onError, options);    
+    }, false);
     function onSuccess(heading) {
-        alert("abc");
         var img = document.getElementById('compass');
-        img.setAttribute('style','transform:rotate('+ heading.magneticHeading + 'deg)');
+        img.setAttribute('style','transform:rotate(-'+ heading.magneticHeading + 'deg)');
+    };
+    function onError(compassError) {
+        alert('Compass error: ' + compassError.code);
+    };
+    
+    var options = {
+        frequency: 1500
     };
 });
+$$(document).on('pageInit', '.page[data-page="result-device-motion"]', function (e) {
+    function onSuccess(acceleration) {
+        let message = "";
+        message += 'Acceleration X: ' + acceleration.x;
+        message += '<br/>Acceleration Y: ' + acceleration.y;
+        message += '<br/>Acceleration Z: ' + acceleration.z;
+        message += '<br/>Timestamp: ' + acceleration.timestamp;
+        document.getElementById('resultDeviceMotion').innerHTML = message;
+    }
+    function onError() {
+        alert('onError!');
+    }
+    var motionOptions = { frequency: 300 };
+    var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, motionOptions);
+    var btnMotionBack = document.getElementById('btnMotionBack');
+    btnMotionBack.addEventListener('click', function(e) {
+        e.preventDefault();
+        navigator.accelerometer.clearWatch(watchID);
+    });
+});
+var btnCheckNetwork = document.getElementById('btnCheckNetwork');
+btnCheckNetwork.addEventListener('click', function(e) {
+    e.preventDefault();
+    var networkState = navigator.connection.type;
+    
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+        
+    alert('Connection type: ' + states[networkState]);
+});
 
+var btnBatteryStatus = document.getElementById('btnBatteryStatus');
+btnBatteryStatus.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.addEventListener("batterystatus", onBatteryStatus, false);
+});
 
-function onError(compassError) {
-    alert('Compass error: ' + compassError.code);
-};
+function onBatteryStatus(status) {
+    alert("Level: " + status.level + " isPlugged: " + status.isPlugged);
+}
+window.addEventListener("batterycritical", onBatteryCritical, false);
+function onBatteryCritical(status) {
+    alert("Battery Level Critical " + status.level + "%\nRecharge Soon!");
+}
+window.addEventListener("batterylow", onBatteryLow, false);
 
-var options = {
-    frequency: 1500
-}; // Update every 3 seconds
-
-
+function onBatteryLow(status) {
+    alert("Battery Level Low " + status.level + "%");
+}
     
     
